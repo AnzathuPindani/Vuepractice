8b2d1d0421c1bd426c0527fb8937e54db37d46a3
AnzathuPindani <anzathupindani286@gmail.com>
2025-07-27 15:30:29 +0200

    Diabled

--- begining/src/App.vue
+++ begining/src/App.vue
@@ -1,11 +1,31 @@
 <script setup>
 import {ref} from 'vue';
 const name = ref('Rachel');
+const href=ref('https://www.scrimba.com');
+const disabled = ref(true);
+const attributes={
+  id:'big_emoji',
+  class:'waving',
+}
+const coding_years=ref(0);
+const getFormattedDate=(date)=>{
+  const options={year:'numeric',month:'long',day:'numeric'};
+  return date.toLocaleDateString('en-UK',options);
+}
+const username=ref('rachelktjohnson');
+const event=ref('dblclick');
+const blockLeave=()=>{
+alert("You can't Leave");
+};
+
 </script>
 
 <template>
   <div>
     <h1>Hi, my name is {{ name }}</h1>
+    <a :href="href" target="_blank">
+      <button :disabled="disabled">Click to visit my favourite website</button>
+    </a>
   </div>
 
 </template>
